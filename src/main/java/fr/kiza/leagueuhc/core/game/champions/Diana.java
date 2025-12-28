12d6d19cd5ab9b8296b4335f023d31dd9e7e5da5
Manawan785 <hugourvoy05@gmail.com>
2025-12-28 13:26:44 +0100

    :tada: champ Diana added

---
+++ src/main/java/fr/kiza/leagueuhc/core/game/champions/Diana.java
@@ -0,0 +1,18 @@
+package fr.kiza.leagueuhc.core.game.champions;
+
+import fr.kiza.leagueuhc.core.api.champion.Champion;
+import fr.kiza.leagueuhc.core.api.champion.annotation.ChampionEntry;
+import org.bukkit.Material;
+
+@ChampionEntry
+public class Diana extends Champion {
+    public Diana() {
+        super(
+                "Diana",
+                "Mépris de la lune",
+                Category.FIGHTER,
+                Region.TARGON,
+                Material.DIAMOND_AXE
+        );
+    }
+}
